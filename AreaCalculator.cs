2da4e31e4fd47891cbf792735f9a8749e1c1d336
Richard <mr.katfish1982@gmail.com>
2024-06-30 01:43:52 -0700

    Commit.

---
+++ AreaCalculator.cs
@@ -0,0 +1,14 @@
+namespace phase1;
+
+public class AreaCalculator
+{
+    private int _height, _width;
+    public AreaCalculator(int h, int w)
+    {
+        _height = h;
+        _width = w;
+    }
+
+    
+    
+}
