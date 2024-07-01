2fb6e2da6294b92ab7a3b6b8d0b947db935dda29
Richard <mr.katfish1982@gmail.com>
2024-06-30 19:35:41 -0700

    Second commit

--- AreaCalculator.cs
+++ AreaCalculator.cs
@@ -1,14 +1,53 @@
+
 namespace phase1;
 
-public class AreaCalculator
+public class AreaCalculator()
 {
-    private int _height, _width;
-    public AreaCalculator(int h, int w)
+    private const double Pi = Double.Pi;
+    private double Height { get; }
+    private double Width { get; }
+    private double Radius { get; }
+    
+    public AreaCalculator(double height, double width, double radius) : this()
     {
-        _height = h;
-        _width = w;
+        Height = height;
+        Width = width;
+        Radius = radius;
     }
 
-    
+    private double Rect()
+    {
+        return Height * Width;
+
+    }
+
+    public double Triangle()
+    {
+        return (Height * Width) / 2;
+    }
+
+    public double Ellipse()
+    {
+        return Pi * Radius * Radius;
+    }
+
+    public double Operation(string op)
+    {
+        double result = double.NaN;
+        switch (op)
+        {
+            case "R":
+                result = Rect();
+                break;
+            case "T":
+                result = Triangle();
+                break;
+            case "E":
+                result = Ellipse();
+                break;
+        }
+        return result;
+    }
+
     
 }
