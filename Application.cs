91b7d99b035006a303c5d5df9d5fcc65a7aa5f1c
Richard <mr.katfish1982@gmail.com>
2024-06-30 22:35:33 -0700

    Commit.

---
+++ Application.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+
+namespace phase1;
+
+class Application
+{
+    private static void Main(string[] args)
+    {
+        var introduction = @"--------Area Calculator-------------
+                                    Here are the available operation codes:
+                                            R = rectangle,
+                                            T = triangle,
+                                            E = ellipse.";
+        Console.WriteLine(introduction);
+        Console.WriteLine("Enter your operation command.");
+        string? operation = Console.ReadLine().ToUpper();
+        Console.WriteLine("Enter the height:");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the width:");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the radius:");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        AreaCalculator calculator = new AreaCalculator(height, width, radius);
+
+        if (operation == null || ! Regex.IsMatch(operation, "[R|T|E]") )
+        {
+            Console.WriteLine("That is not valid");
+        }
+        else
+        {
+            try
+            {
+                double result = calculator.Operation(operation);
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("That is invalid.");
+                }
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception occurred trying to do the math.\n - Details: "
+                                  + e.Message);
+                
+            }
+        }
+    }
+}
